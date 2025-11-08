c0a1e086354dc5017e21c78bcf551cf98c972b8e
German <aleksss10000@gmail.com>
2025-11-09 00:25:29 +0300

    Projects cards | Username/ava

--- aiser/src/pages/Account.jsx
+++ aiser/src/pages/Account.jsx
@@ -1,23 +1,43 @@
 import '../static/account.css'
 import '../static/global.css'
 import { Link } from 'react-router-dom'
+import MainButton from './components/MainButton'
+import { useState } from 'react'
 
 export default function Account(){
+    const [isListVisible, setIsListVisible] = useState(false)
 
+    const toggleList = () => {
+        setIsListVisible(!isListVisible)
+    }
     return(
         <div id="travoman">
             <div className='homelink'><Link to='/' className='home'>Главная</Link></div>
             <div id="account">
                 <aside id="accountpanel">
-                    <div id="ava">
-
+                    <div id="user">
+                        <div id='ava'></div>
+                        <div id='user-name'></div>
+                        
+                    </div>
+                    <div id='projects'>
+                        <button id='buttproj' onClick={toggleList}><p id='butt-sign' >Проекты</p></button>
+                        <ul id='list' className={isListVisible ? 'visible' : 'hidden'}>
+                            <li>ТГ-бот для пивнухи</li>
+                        </ul>
                     </div>
                 </aside>
-                <div id='startdiv'><button id="start-test-button">Начать тест</button></div>
-                <div className='cardsdiv' id='firstdiv'>
-                    <div className='card'>
 
+                <div className='cardsdiv' id='firstdiv'>
+
+                    <div className='card' id='card1'>
+                        <div id='card-name-div'><p id='card-name'>ТГ-бот для пивнухи</p></div>
+                        <div id='bottom-row'>
+                            <Link to='/' id='gear'/>
+                            <div id='indicator'></div>
+                        </div>
                     </div>
+
                     <div className='card'>
 
                     </div >
